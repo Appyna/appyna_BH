45ed80522b6a1c2858faf8f0d08a305eb4dde2c4
Nicolas <nicolaslpa@Mac.lan>
2025-11-19 15:33:51 +0100

    style: remove exclamation mark from success message for better mobile display

--- components/BoostListingModal.tsx
+++ components/BoostListingModal.tsx
@@ -84,7 +84,7 @@ export const BoostListingModal: React.FC<BoostListingModalProps> = ({
                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
               </svg>
               <p className="text-green-800 font-bold text-base sm:text-lg font-poppins">
-                Annonce publiée avec succès !
+                Annonce publiée avec succès
               </p>
             </div>
           </div>
